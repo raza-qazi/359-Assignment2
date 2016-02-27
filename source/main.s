e090cdcb8afbab6e3dbc362b4d8a94bfdbed6cb9
Raza Qazi <raza.qazi@ucalgary.ca>
2016-02-26 17:43:44 -0700

    almost final sourcecode

--- source/main.s
+++ source/main.s
@@ -46,16 +46,34 @@ nameCHK:
 	mov	r1, #39
 	bl	WriteStringUART		// Print numStudent
 
+        // Clear buffer
+        ldr     r0, = ABuff
+        strb    r12, [r0]
+
 	ldr	r0, = ABuff	        // Store address
 	mov	r1, #1     	       	// Number of chars
 	bl	ReadLineUART		// ABuff now stores input
 
 	ldr	r0, = ABuff
 	ldrb	r3, [r0]		// ldrb - take ONE byte
-	sub	r3, #48		     	// r2 now stores num of students
 
-	cmp	r2, #1
-	blt	errorINVS
+        cmp     r3, #48
+        blt     errorINVS               // Special case: character NOT b/w 0-9
+        cmp     r3, #58
+        bge     errorINVS
+
+	sub	r3, #48		     	// r3 now stores num of students
+
+        ldr     r0, = newLineStr        // Print newLineStr
+        mov     r1, #2
+        bl      WriteStringUART
+
+        cmp     r0, #0                   // Special case: no input
+        beq     errorINVS
+	cmp	r3, #0                   // Special case: numStudent = 0
+	beq	errorINVS
+
+
 	b	continue
 
 errorINVS:
@@ -69,7 +87,6 @@ continue:
         ldr     r11, = GradeStr         // Setup address of GradeStr into r11 for use in loop
 
         mov     r9, #0                  // totalSum = 0
-        mov     r13, #0                 // Average counter
 // For loop begins here
 Loop:
 	cmp     r4, r3                  // while(i < numStudent)
@@ -82,10 +99,13 @@ Loop:
 
         mov     r8, #0                  // r8 is result
 
-
         mov     r0, r11                 // Updated GradeStr address in r0
 	mov	r1, #36
 	bl	WriteStringUART		// Print GradeStr for each iteration
+
+        ldr     r0, = arrowStr          // Print arrowStr
+        mov     r1, #1
+        bl      WriteStringUART
 
         ldr	r0, = ABuff	        // Store address
 	mov	r1,#256		       	// Number of chars
@@ -97,12 +117,13 @@ Loop:
 
         cmp     r0, #0
         beq     errorINVG
-        // Assuming 3 digits with the first two possibly zero [r0] = Xxx, [r0, #1] = xXx, [r0, #2] = xxX
+
+        // Assuming 3 digits with the first two possibly zero [r0]
 	ldr	r0, = ABuff	        // Store address back into r0
         ldrb    r7, [r0, #2]            // string at first place
         ldrb    r6, [r0, #1]            // String at second place
         ldrb    r5, [r0]                // String at third place
-test2:
+
 	// From notes
 	cmp	r5, #58			// is it any ascii value beyond 9?
 	bge	wrongType		// If so, branch to errorMsg
@@ -119,7 +140,7 @@ test2:
         sub     r6, #48                 // Number as an int
         sub     r7, #48                 // Number as an int
 
-test:	add     r8, r7                  // r8 = r8 (0) + firstDigit
+	add     r8, r7                  // r8 = r8 (0) + firstDigit
 	mov	r10, #10		// Constant 10
         mul     r6, r10                 // r6 = r6*10 - digit at 2nd place
         add     r8, r6                  // r8 = first digit + second digit
@@ -134,8 +155,9 @@ test:	add     r8, r7                  // r8 = r8 (0) + firstDigit
         bgt	errorINVG
 
 	b	totalSum
+
 twoDigit:
-	cmp	r5, #0
+	cmp	r6, #0
 	beq	oneDigit
 
 	sub     r5, #48                 // Number as an int
@@ -145,11 +167,13 @@ twoDigit:
 	mla	r8, r5, r10, r6		// Need to multiply by 10: r7 = r6*r10 (#10) + r5 = 2 digit num
                                         // No error check needed
         b       totalSum
-oneDigit:
-        sub     r7, #48                 // Number as an int
 
-        add     r8, r7
+oneDigit:
+        sub     r5, #48                 // Number as an int
+
+        add     r8, r5
         b       totalSum
+
 wrongType:
         ldr     r0, = wrongNum
         mov     r1, #22
@@ -171,12 +195,12 @@ totalSum:
         // Assuming total value is final
 continue2:
 	mov	r0, r9			// Temp value to store total sum
-	mov	r10, r2			// temp to store numstudent
-	mov	r4, #0			// increment i
+	mov	r10, r3			// temp to store numstudent
+	mov	r4, #0			// increment i = 0
 
 avgCalc:
         cmp     r0, #0                  // Doc
-        beq     dispOutput	                 //
+        beq     convert	                 //
         blt     decrement
 
         sub	r0, r10			 // Subtract from total, numStudent
@@ -186,17 +210,113 @@ avgCalc:
 
 	b	avgCalc			// Go to top and compare
 
-decrement:				// Applicable if the divison is has a remainder
+decrement:				// Applicable if the divison has a remainder
 	sub	r4, #1
 
-dispOutput:
+convert:
+        mov     r0, r9
+        stmfd   sp!,{r0}              // Converting totalSum to string
+        BL      itoa                    // Store into buffer as string
 
-	ldr	r0, = totalSumStr
+        ldr	r0, = totalSumStr
 	mov	r1, #12
 	bl	WriteStringUART
 
+        ldr     r0, = numStr
+        mov     r1, #8
+        bl      WriteStringUART
+
+        mov     r0, r4
+        stmfd   sp!,{r0}              // Converting average to string
+        BL      itoa                  // Store into buffer as string
+
+        ldr     r0, = totalAvg
+        mov     r1, #16
+        bl      WriteStringUART
+
+        ldr     r0, = numStr
+        mov     r1, #8
+        bl      WriteStringUART
+
+        b       nameCHK                // Loop back to the start
 haltLoop$:
 	b	haltLoop$
+
+// SUBROUTINES
+itoa:   LDMFD   sp!, {r6}
+
+        mov     r0, #0                  // least sig
+        mov     r1, #0                  // second sig
+        mov     r2, #0                  // third sig
+        mov     r3, #0                  // counter
+        mov     r7, #0                  // Addresses
+        mov     r10, #32                 // add space
+
+
+        ldr     r7, = numStr            // Preparing to reset numStr
+        strb    r10, [r7, #2]           // Reset the buffer to zero
+        strb    r10, [r7, #1]
+        strb    r10, [r7]
+
+        cmp     r6, #10
+        blt     ones
+        cmp     r6, #100
+        blt     tens
+
+        // caseOne
+hundreds:                                // 3 digit num
+        cmp     r6, #100
+        blt     tens
+
+        sub     r6, #100                 // Take out 100
+        add     r2, #1                  // increase count of 100s
+
+        b       hundreds
+tens:
+        cmp     r6, #10
+        blt     ones
+
+        sub     r6, #10
+        add     r1, #1
+
+        b       tens
+ones:
+        mov     r0, r6                  // r0 = r6
+
+        cmp     r2, #0
+        beq     svTwo
+
+        add     r0, #48
+        add     r1, #48
+        add     r2, #48
+
+        ldr     r7, = numStr             // Write to buffer
+        // Assuming 134 is the number to be printed
+        strb    r0, [r7, #2]              // Stores 4
+        strb    r1, [r7, #1]             // Stores 3
+        strb    r2, [r7]                // Stores 1
+        b       cont
+
+svTwo:
+        cmp     r1, #0
+        beq     svOne
+
+        add     r0, #48
+        add     r1, #48
+
+        ldr     r7, = numStr            // Write to buffer
+        // Assuming 134 is the number to be printed
+        strb    r0, [r7, #1]            // Stores 4
+        strb    r1, [r7]                // Stores 3
+
+        b       cont
+svOne:
+        add     r0, #48
+
+        ldr     r7, = numStr             // Write to buffer
+        strb    r0, [r7]              // Stores 4
+cont:
+        mov     pc, lr                 // Return
 
 .section .data
 
@@ -227,7 +347,11 @@ totalAvg:   // Char: 16
 newLineStr:    // Char: 2
 	.ascii	"\n\r"
 
+arrowStr:     // Char: 1
+        .ascii  ">"
 
+numStr:    // Char: 8
+        .ascii  "      \n\r"
 ABuff:
 	.rept	256
 	.byte	0
